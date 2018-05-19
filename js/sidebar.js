c6af9e554e790c1581cd585aae03d346861934e6
icefeng <862628057@qq.com>
2018-05-19 16:10:31 +0800

    test

--- js/sidebar.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    $("a#menu-toggle").on('click', sidebarToggle);
-
-    function sidebarToggle(){
-        $("body").toggleClass("toggled");
-        $('a#menu-toggle').find('i.fa').toggleClass('fa-bars').toggleClass('fa-times');
-    }
-} ());
